e19e48da6c5a383bb9a78e893747ff4c67026e8c
Eshwari Borade <eshwari.borade@cumminscollege.in>
2024-03-08 18:48:01 +0530

    i have created 1 file

---
+++ sum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Sum {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+
+        System.out.println("Addition= "(a+b));
+        
+    }
+}
